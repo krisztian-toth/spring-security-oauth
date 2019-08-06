13f66eeb482f49d1c3fe8b6288f774a1e56a0b40
Krisztian Toth <krisztian.toth@cgi.com>
2019-08-06 14:06:42 +0200

    enable web security for the application

--- src/main/java/hu/krisz/config/SecurityConfig.java
+++ src/main/java/hu/krisz/config/SecurityConfig.java
@@ -3,14 +3,18 @@ package hu.krisz.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
+ * Security configuration for the authorization server.
+ *
  * @author krisztian.toth on 5-8-2019
  */
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
