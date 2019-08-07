2bad002548f0e4357ba073f6e33371984d936810
Krisztian Toth <krisztian.toth@cgi.com>
2019-08-07 09:52:05 +0200

    update authorization server configuration to store tokens as JWT with basic implementation

--- src/main/java/hu/krisz/config/AuthServerConfig.java
+++ src/main/java/hu/krisz/config/AuthServerConfig.java
@@ -13,7 +13,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
@@ -49,6 +53,26 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
         return new BCryptPasswordEncoder(B_CRYPT_LOG_ROUNDS);
     }
 
+    @Bean
+    protected JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("123321");
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    protected TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    @Bean
+    protected DefaultTokenServices defaultTokenServices(TokenStore tokenStore) {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore);
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.passwordEncoder(passwordEncoder()).allowFormAuthenticationForClients();
@@ -58,6 +82,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(new JdbcTokenStore(dataSource))
+                .accessTokenConverter(accessTokenConverter())
                 .userDetailsService(userDetailsService())
                 .authenticationManager(authenticationConfiguration.getAuthenticationManager());
     }
