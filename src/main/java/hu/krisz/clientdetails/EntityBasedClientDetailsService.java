2e595b7282a1d8e06b9a6fb0174fcc1dd2e1b1da
Krisztian Toth <krisztian.toth@cgi.com>
2019-08-14 15:29:06 +0200

    provide implementation of ClientDetailsService

---
+++ src/main/java/hu/krisz/clientdetails/EntityBasedClientDetailsService.java
@@ -0,0 +1,35 @@
+package hu.krisz.clientdetails;
+
+import hu.krisz.dao.repository.ClientRepository;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.NoSuchClientException;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+
+/**
+ * An implementation of {@link ClientDetailsService} which uses Spring Data JPA repositories.
+ *
+ * @author krisztian.toth on 14-8-2019
+ */
+public class EntityBasedClientDetailsService implements ClientDetailsService {
+
+    private final ClientRepository clientRepository;
+
+    public EntityBasedClientDetailsService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
+        return clientRepository.findById(clientId)
+                .map(client -> new BaseClientDetails(
+                        client.getClientId(),
+                        client.getResourceIds(),
+                        client.getScopes(),
+                        client.getAuthorizedGrantTypes(),
+                        client.getAuthorities(),
+                        client.getWebServerRedirectUri()
+                )).orElseThrow(() -> new NoSuchClientException("No client found with id=" + clientId));
+    }
+}
