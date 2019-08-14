c35f55cd0eae7102c3de2be9ff652494c293835f
Krisztian Toth <krisztian.toth@cgi.com>
2019-08-14 14:10:49 +0200

    add missing getter to OAuthAuthority

--- src/main/java/hu/krisz/dao/entity/client/OAuthAuthority.java
+++ src/main/java/hu/krisz/dao/entity/client/OAuthAuthority.java
@@ -26,6 +26,10 @@ public class OAuthAuthority {
     private OAuthAuthority() {
     }
 
+    public String getAuthority() {
+        return authority;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
