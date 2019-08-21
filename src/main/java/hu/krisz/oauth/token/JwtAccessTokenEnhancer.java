3c21188ecf2172d6141456f866ea4d359642e7d7
Krisztian Toth <krisztian.toth@cgi.com>
2019-08-21 14:33:27 +0200

    improve token endpoint result

--- src/main/java/hu/krisz/oauth/token/JwtAccessTokenEnhancer.java
+++ src/main/java/hu/krisz/oauth/token/JwtAccessTokenEnhancer.java
@@ -6,6 +6,8 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
+import java.util.Collections;
+
 /**
  * Converts the access token to a JWT while not modifying the refresh token itself.
  *
@@ -24,6 +26,9 @@ public class JwtAccessTokenEnhancer implements TokenEnhancer {
         OAuth2AccessToken enhancedAccessToken = accessTokenConverter.enhance(accessToken, authentication);
         DefaultOAuth2AccessToken result = new DefaultOAuth2AccessToken(enhancedAccessToken);
         result.setRefreshToken(accessToken.getRefreshToken());
+
+        // as this map is included in the token response we replace it with an empty map
+        result.setAdditionalInformation(Collections.emptyMap());
         return result;
     }
 }
